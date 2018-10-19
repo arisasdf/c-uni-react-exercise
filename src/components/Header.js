db31f365564020f8faa2f6ea4b75db8956d79f9c
Eme_Effiom <EffiomEU@cardiff.ac.uk>
2018-10-19 16:04:40 +0100

    Create Header Components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,45 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import "../img/Menuicon.svg";
 
 class Header extends Component {
   render() {
     return (
-      <header></header>
+      <header>
+        <nav className="mainMenu">
+          <span className="logo">
+            E &amp; L <i>tutorials</i>
+          </span>
+          <ul>
+            <li>
+              <a href="">HOME</a>
+            </li>
+            <li>
+              <a href="">ABOUT US</a>
+              <ul>
+                <li>About E&amp;L</li>
+                <li>
+                  <a>Profile 1</a>
+                </li>
+                <li>
+                  <a>Profile 2</a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a href="">VIDEO TUTORIALS</a>
+            </li>
+            <li>
+              <a href="">LEARNING RESOURCES</a>
+            </li>
+            <li>
+              <a href="">EXTERNAL LINKS</a>
+            </li>
+            <li>
+              <a href="">BLOG</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
     );
   }
 }

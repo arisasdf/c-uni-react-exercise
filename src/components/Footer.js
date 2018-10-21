fc37b01a6abb2186ee9d8fde60e0e4b4236ce09c
Lucia S <serranocortesl@cardiff.ac.uk>
2018-10-21 23:43:25 +0100

    Global page footer - Added HTML content to the footer component - Updated CSS for the footer and responsiveness - Removed unnecessary (empty) CSS files

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
 
 class Footer extends Component {
-  render() {
-    return (
-      <footer></footer>
-    );
-  }
+	render() {
+		return (
+			<footer class="global-footer">
+				<div class="global-footer__block">
+					<h4>Pages</h4>
+					<ul>
+						<li><a href="#">Learning Resources</a></li>
+						<li><a href="#">Video Tutorials</a></li>
+						<li><a href="#">External Links</a></li>
+					</ul>
+				</div>
+				<div class="global-footer__block">
+					<h4>Team</h4>
+					<ul>
+						<li><a href="#">About Us</a></li>
+						<li><a href="#">Blog</a></li>
+						<li><a href="#">Contact</a></li>
+					</ul>
+				</div>
+				<div class="global-footer__copyright">Lucia S. &amp; Eme E. for the NSE - Cardiff University</div>
+			</footer>
+		);
+	}
 }
 
 export default Footer;

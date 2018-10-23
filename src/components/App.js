a46738f4e7911114c6dd0edc12decf0b54ebbd5b
Eme_Effiom <EffiomEU@cardiff.ac.uk>
2018-10-23 13:06:47 +0100

    Update Header Elements - Add react router to package.json - Add hamburger menu image - Add router switch to App.js - Add router links to header elements in Header.js - Create hamburger menu - Add responsive styles for header component

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,19 @@
-import React, { Component } from 'react';
-
-import Header from './Header';
-import Footer from './Footer';
+import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
+import Header from "./Header";
+import Home from "./Home";
+import Footer from "./Footer";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+
+        <Switch>
+          <Route exact path="/Home" component={Home} />
+        </Switch>
+
         <Footer />
       </div>
     );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,36 +1,51 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+function myFunction() {
+  var x = document.getElementById("menu_Items");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
 
 class Header extends Component {
   render() {
     return (
       <header>
         <nav className="mainMenu">
+          <button type="button" onClick={myFunction}>
+            <div>
+              <img src={"/menu.png"} alt="menu" width="30" height="35" />
+            </div>
+          </button>
+
           <span className="logo">
             E &amp; L <i>tutorials</i>
           </span>
-          <ul>
-            <li>HOME</li>
-            <li>
-              ABOUT US
-              <ul className="about">
-                <li>About E&amp;L</li>
-                <li>Profile 1</li>
-                <li>Profile 2</li>
-              </ul>
-            </li>
-            <li class="videos">VIDEO TUTORIALS</li>
-            <li class="resources">LEARNING RESOURCES</li>
-            <li class="ext_links">EXTERNAL LINKS</li>
-            <li class="mobile">
-              MORE
-              <ul class="more">
-                <li>Video Tutorials</li>
-                <li>Learning Resources</li>
-                <li>External Links</li>
-              </ul>
-            </li>
-            <li>BLOG</li>
-          </ul>
+          <div id="menu_Items">
+            <ul>
+              <li>
+                <Link to="/Home">HOME</Link>
+              </li>
+              <li>
+                <Link to="/Home">ABOUT US</Link>
+              </li>
+              <li class="videos">
+                <Link to="/Home">VIDEO TUTORIALS</Link>
+              </li>
+              <li class="resources">
+                <Link to="/Home">LEARNING RESOURCES</Link>
+              </li>
+              <li class="ext_links">
+                <Link to="/Home">EXTERNAL LINKS</Link>
+              </li>
+              <li>
+                <Link to="/Home">BLOG</Link>
+              </li>
+            </ul>
+          </div>
         </nav>
       </header>
     );

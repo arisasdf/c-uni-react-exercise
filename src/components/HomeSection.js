6ef5f40ccde53cf911c4f78e492d6d10615bfdd0
Lucia S <serranocortesl@cardiff.ac.uk>
2018-10-24 16:51:37 +0100

    Learning Resources Home Section - Added a HomeSection container component - Added the box components - Responsive CSS for the box row - JSON with initial Learning Resources data

---
+++ src/components/HomeSection.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import ResourceBox from './ResourceBox';
+import MoreBox from './MoreBox';
+
+class HomeSection extends Component {
+
+  render() {
+    const resourceBoxes = this.props.items.map((r) => <ResourceBox title={r.title} category={r.category} author={r.author} slug={r.slug} />);
+
+    return (
+    	<section class="home-section">
+    		<h2>{this.props.title}</h2>
+        <div class="resource__row">
+          { resourceBoxes }
+          <MoreBox section={this.props.section} />
+        </div>
+      </section>
+    );
+  }
+}
+
+export default HomeSection;
---
+++ src/components/MoreBox.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class MoreBox extends Component {
+	render() {
+		return (
+			<a class="resource__box resource__box--more" href={'/' + this.props.section}>
+				<img src="/img/thumb/more.png" />
+				<h3>More</h3>
+			</a>
+		);
+	}
+}
+
+export default MoreBox;

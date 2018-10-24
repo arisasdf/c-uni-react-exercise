3351638c8cbfd0fbeaf06d27e0f387a1858c0743
Eme Effiom <EffiomEU@cardiff.ac.uk>
2018-10-24 14:47:26 +0100

    Remove unlinked routes

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,7 @@ import { Switch, Route } from "react-router-dom";
 import Header from "./Header";
 import Home from "./Home";
 import About from "./About";
-import Video_Tutorials from "./Video_Tutorials";
 import External_Links from "./External_Links";
-import Learning_Resources from "./Learning_Resources";
 import Profile1 from "./Profile1";
 import Profile2 from "./Profile2";
 import Footer from "./Footer";
@@ -18,10 +16,7 @@ class App extends Component {
 
         <Switch>
           <Route exact path="/Home" component={Home} />
-
-          <Route path="/Video_Tutorials" component={Video_Tutorials} />
           <Route path="/External_Links" component={External_Links} />
-          <Route path="/Learning_Resources" component={Learning_Resources} />
           <Route exact path="/Profile1" component={Profile1} />
           <Route exact path="/Profile2" component={Profile2} />
           <Route path="/About" component={About} />

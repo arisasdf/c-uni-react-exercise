f68411486c48dcd2b9a46c842c3e59ad46990707
Eme Effiom <EffiomEU@cardiff.ac.uk>
2018-10-25 01:24:37 +0100

    Add Page Content

--- src/components/External_Links.js
+++ src/components/External_Links.js
@@ -8,21 +8,89 @@ class External_links extends Component {
         <main>
           <div className="flex-container External_links">
             <div>
-              <h2>STACK OVERFLOW</h2>
+              <h1>YOUTUBE</h1>
               <p>
                 Full stack development in Java
                 <br />
-                <Link to="/Home">
-                  www.stackoverflow.com/full_stack_development_in java
-                </Link>
+                <a href="https://www.youtube.com/watch?v=WPvGqX-TXP0">
+                  https://www.youtube.com/watch?v=WPvGqX-TXP0
+                </a>
               </p>
             </div>
-            <div />
-            <div />
-            <div />
-            <div />
-            <div />
-            <div />
+            <div>
+              <h1>DAVID J ECK</h1>
+              <p>
+                Introduction to Programming Using Java, Seventh Edition Version
+                7.0, August 2014
+                <br />
+                <a href="http://math.hws.edu/javanotes/">
+                  http://math.hws.edu/javanotes/
+                </a>
+              </p>
+            </div>
+            <div>
+              <h1>YOUTUBE</h1>
+              <p>
+                Javascript for beginers
+                <br />
+                <a href="https://www.youtube.com/watch?v=W6NZfCO5SIk">
+                  https://www.youtube.com/watch?v=W6NZfCO5SIk
+                </a>
+              </p>
+            </div>
+            <div>
+              <h1>JAVASCRIPT.INFO</h1>
+              <p>
+                The Modern Javascript Tutorials
+                <br />
+                <a href="https://javascript.info/">https://javascript.info/</a>
+              </p>
+            </div>
+
+            <div>
+              <h1>YOUTUBE</h1>
+              <p>
+                Coding in HTML5 for beginers
+                <br />
+                <a href="https://www.youtube.com/watch?v=eo3ujSPsfGo">
+                  https://www.youtube.com/watch?v=eo3ujSPsfGo
+                </a>
+              </p>
+            </div>
+            <div>
+              <h1>JOHN DUCKETT</h1>
+              <p>
+                HTML & CSS Design and build websites
+                <br />
+                <a
+                  href="http://www.wufai.edu.tw/%E7%B6%B2%E9%A0%81%E6%8A%80%E8%A1%93%E4%B8%AD%E5%BF%83/datasheet/HTML%20and%20CSS%20design%20and%20build%20websites.pdf
+               "
+                >
+                  http://www.wufai.edu.tw/%E7%B6%B2%E9%A0%81%E6%8A%80%E8%A1%93%E4%B8%AD%E5%BF%83/datasheet/HTML%20and%20CSS%20design%20and%20build%20websites.pdf
+                </a>
+              </p>
+            </div>
+
+            <div>
+              <h1>YOUTUBE</h1>
+              <p>
+                CSS Crash course for absolute beginers
+                <br />
+                <a href="https://www.youtube.com/watch?v=yfoY53QXEnI">
+                  https://www.youtube.com/watch?v=yfoY53QXEnI
+                </a>
+              </p>
+            </div>
+            <div>
+              <h1>O'REILLY</h1>
+              <p>
+                Microservices for Java Developers
+                <br />
+                <a href="https://www.oreilly.com/library/view/microservices-for-java/9781492042228/">
+                  https://www.oreilly.com/library/view/microservices-for-java/9781492042228/
+                </a>
+              </p>
+            </div>
           </div>
         </main>
       </div>
